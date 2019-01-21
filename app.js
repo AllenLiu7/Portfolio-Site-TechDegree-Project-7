6530eccbdaf1e3d7ee51d9910d335ee0a0133179
AllenLiu7 <41953650+AllenLiu7@users.noreply.github.com>
2019-01-21 13:55:37 +0800

    add code for pug

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const app = express();
-const { projects } = require('./data/projects.json');
+
+const { projects } = require('./data.json');
+
+
 app.use('/static', express.static('public'));
 
 
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-    res.render('index');
+    res.render('index', {projects});
 })
 
 app.get("/about", (req, res) => {
     res.render('about');
 })
 
-app.get("/project", (req, res) => {
-    res.render('project');
+app.get("/:id", (req, res) => {
+    res.render('project', {
+        projects,
+        index: req.params.id
+    });
 })
 
 app.listen(3000, () => {
