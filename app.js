1ac77eb8565421d78e97362b3547fc2c59b52f6b
Allen Liu <allen6qi@gmail.com>
2019-01-28 12:52:41 +0800

    add procfile, change port setting and add new port variable to views

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
 
 const { projects } = require('./data.json');
 
@@ -10,20 +15,20 @@ app.use('/static', express.static('public'));
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-    res.render('index', {projects});
+    res.render('index', {projects, port});
 })
 
 app.get("/about", (req, res) => {
-    res.render('about');
+    res.render('about', {port});
 })
 
 app.get("/projects/:id", (req, res) => {
     res.render('project', {
-        projects: projects,
-        index: req.params.id
+        projects,
+        index: req.params.id,
+        port
     });
 })
 
-app.listen(3000, () => {
-    console.log("server running on port 3000");
-});
+
+app.listen(port);
