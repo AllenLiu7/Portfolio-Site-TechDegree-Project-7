5baaf068273d9215a3818e3e4a434df6eda219d6
Allen Liu <allen6qi@gmail.com>
2019-01-28 13:06:17 +0800

    change main route

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('static', express.static('public'));
 
 app.set("view engine", "pug");
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
     res.render('index', {projects, port});
 })
 
