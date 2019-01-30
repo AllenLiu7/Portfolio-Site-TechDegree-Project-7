9a367b828379dbca8106979247710f415b9dae87
Allen Liu <allen6qi@gmail.com>
2019-01-30 12:35:53 +0800

    add example html, add comments, adjust CSS, add some text

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
+
+//adjust to correct code when using Heroku
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
 
-
+// link to data.json, set it to variable 'projects'
 const { projects } = require('./data.json');
 
-
+//link to public folder for static setting
 app.use('/static', express.static('public'));
 
-
+//use pug as engine
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
@@ -22,6 +24,7 @@ app.get("/about", (req, res) => {
     res.render('about', {port});
 })
 
+//set :id to the 'index' variable for dynamic setting in pug
 app.get("/projects/:id", (req, res) => {
     res.render('project', {
         projects,
@@ -30,12 +33,14 @@ app.get("/projects/:id", (req, res) => {
     });
 })
 
+//set up the 'not found' error
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 })
 
+//render the error page
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
@@ -43,4 +48,4 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(port);
+app.listen(port, () => console.log(`listening on port ${port}!`));
